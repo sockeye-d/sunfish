ae517fa66252dd8f0a5c8ec9d868a14f8ec9fd1f
fish <me@fishies.dev>
2025-11-02 01:24:37 -0700

    it don't work

--- addons/plugin_exporter/plugin_export_platform.gd
+++ addons/plugin_exporter/plugin_export_platform.gd
@@ -16,6 +16,21 @@ func _get_logo() -> Texture2D:
 func _get_platform_features() -> PackedStringArray: return ["plugin"]
 
 
+func _get_export_options() -> Array[Dictionary]:
+	return [
+		{
+			"name": "include_core_plugins",
+			"type": TYPE_BOOL,
+		},
+	]
+
+
+func _get_binary_extensions(preset: EditorExportPreset) -> PackedStringArray: return ["zip"]
+
+
+func _get_preset_features(preset: EditorExportPreset) -> PackedStringArray: return []
+
+
 func _can_export(preset: EditorExportPreset, debug: bool) -> bool:
 	return true
 
@@ -27,7 +42,7 @@ func _export_project(preset: EditorExportPreset, debug: bool, path: String, flag
 		return open_err
 	export_project_files(preset, debug,
 		func(file_path: String, file_data: PackedByteArray, file_index: int, file_count: int, encryption_include_filters: PackedStringArray, encryption_exclude_filters: PackedStringArray, encryption_key: PackedByteArray):
-			if file_path.begins_with("res://icons") or file_path == "res://project.binary":
+			if file_path.begins_with("res://icons"):
 				return
 			zip.start_file(file_path.trim_prefix("res:/"))
 			zip.write_file(file_data)
